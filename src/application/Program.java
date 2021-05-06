e2e6545606c703e94a3965bff4f2e1c300ad49de
THASSIUS <thassius.carrion@hotmail.com>
2021-05-06 15:29:41 -0300

    criando metodo findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,9 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,9 +13,16 @@ public class Program {
 		
 		SellerDao sDao = DaoFactory.createSellerDao();
 		
+		System.out.println("==== Test 1: seller findById ====");
 		System.out.println(sDao.findById(3));
 		
+		System.out.println("\n==== Test 2: seller findByDepartment ====");
+		Department dep = new Department(2, null);
+		List<Seller> list = sDao.findByDepartment(dep);
 		
+		for(Seller seller : list) {
+			System.out.println(seller);
+		}
 		
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -43,7 +46,6 @@ public class SellerDaoJDBC implements SellerDao{
 		ResultSet rs = null;
 		PreparedStatement st = null;
 		
-		
 		try {
 			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName " 
 					+ "FROM seller INNER JOIN department "
@@ -52,22 +54,11 @@ public class SellerDaoJDBC implements SellerDao{
 			
 			st.setInt(1, id);
 			rs = st.executeQuery();
-			
 			if(rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				Seller seller = new Seller();
-				seller.setDepartment(dep);
-				seller.setId(rs.getInt("Id"));
-				seller.setName(rs.getString("Name"));
-				seller.setEmail(rs.getString("Email"));
-				seller.setBirthDate(rs.getDate("BirthDate"));
-				seller.setBaseSalary(rs.getDouble("BaseSalary"));
-				
+				Department dep = implementingDep(rs);
+				Seller seller = implementingSeller(rs, dep);
 				return seller;
 			}
-			
 			return null;
 		}
 		catch (SQLException e) {
@@ -80,9 +71,68 @@ public class SellerDaoJDBC implements SellerDao{
 	}
 
 	@Override
+	public List<Seller> findByDepartment(Department dep) {
+		ResultSet rs = null;
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name ");
+			
+			st.setInt(1, dep.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while(rs.next()) {
+				
+				Department department = map.get(rs.getInt("DepartmentId"));
+				
+				if (department == null) {
+					department = implementingDep(rs);
+					map.put(rs.getInt("DepartmentId"), department);
+				}
+				
+				Seller seller = implementingSeller(rs, department);
+				list.add(seller);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+	
+	@Override
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private Department implementingDep(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
+	
+	private Seller implementingSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller seller = new Seller();
+		seller.setDepartment(dep);
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBirthDate(rs.getDate("BirthDate"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		return seller;
 	}
 
 }
