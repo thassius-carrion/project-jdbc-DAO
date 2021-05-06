1a0ba273ce7a58432f88017b4467109f9ff3c1a0
THASSIUS <thassius.carrion@hotmail.com>
2021-05-06 16:33:50 -0300

    criando metodo insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -19,18 +20,20 @@ public class Program {
 		System.out.println("\n==== Test 2: seller findByDepartment ====");
 		Department dep = new Department(2, null);
 		List<Seller> list = sDao.findByDepartment(dep);
-		
 		for(Seller seller : list) {
 			System.out.println(seller);
 		}
 		
 		System.out.println("\n==== Test 3: seller findByAll ====");
 		List<Seller> listAll = sDao.findAll();
-		
 		for(Seller seller : listAll) {
 			System.out.println(seller);
 		}
 		
+		System.out.println("\n==== Test 4: seller insert ====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
